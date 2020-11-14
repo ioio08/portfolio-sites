ff32accff6f63852768ff940525f91b3273da278
Iori <iki.16b@gmail.com>
2020-11-15 03:59:00 +0900

    Add vuex + Add route.id filter

--- pages/dynamics/_id/index.vue
+++ pages/dynamics/_id/index.vue
@@ -5,10 +5,10 @@
     <v-container style="padding-top:0;">
       <v-row justify="center" align="center">
         <v-col cols="12" sm="6">
-          <v-card-title>サイトタイトル{{ $route.params.id }} </v-card-title>
-          <v-img src="/images/admin.jpeg"></v-img>
+          <v-card-title><h2>{{ dynamics.title }} </h2> </v-card-title>
+          <v-img style="width:700px" :src="dynamics.img"></v-img>
           <v-card-actions style="justify-content:flex-end">
-            <v-btn>リンク</v-btn>
+            <v-btn :href="dynamics.to">リンク</v-btn>
           </v-card-actions>
         </v-col>
         <v-col cols="12" sm="6">
@@ -17,27 +17,20 @@
           <v-card-subtitle>使用言語</v-card-subtitle>
           <v-card-text>
             <v-row justify="center">
-              <v-col cols="12" sm="2">
-                <v-img style="width:25px;" src="https://cdn.svgporn.com/logos/html-5.svg"></v-img>
-              </v-col>
-              <v-col cols="12" sm="2">
-                <v-img style="width:25px;" src="https://cdn.svgporn.com/logos/html-5.svg"></v-img>
-              </v-col>
-              <v-col cols="12" sm="2">
-                <v-img style="width:25px;" src="https://cdn.svgporn.com/logos/html-5.svg"></v-img>
-              </v-col>
-              <v-col cols="12" sm="2">
-                <v-img style="width:25px;" src="https://cdn.svgporn.com/logos/html-5.svg"></v-img>
-              </v-col>
-              <v-col cols="12" sm="2">
-                <v-img style="width:25px;" src="https://cdn.svgporn.com/logos/html-5.svg"></v-img>
+              <v-col
+              cols="12"
+              sm="2"
+              v-for="lang in dynamics.useLangs"
+              :key="lang.icon"
+              >
+                <v-img :style="lang.width" :src="lang.icon"></v-img>
               </v-col>
             </v-row>
           </v-card-text>
           <v-divider></v-divider>
           <v-card-subtitle>出来る事</v-card-subtitle>
           <v-card-text>
-            <v-sheet>このサイトは、、、こんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんなこんな</v-sheet>
+            <v-sheet></v-sheet>
           </v-card-text>
         </v-col>
       </v-row>
@@ -46,8 +39,14 @@
 </template>
 
 <script>
-  export default {
-    props:['dynamics'],
+import { mapGetters } from 'vuex';
 
+  export default {
+    computed: {
+      ...mapGetters(['getDynamicsData']),
+      dynamics: function () {
+        return this.getDynamicsData(this.$route.params.id);
+      },
+    },
   }
 </script>
--- store/index.js
+++ store/index.js
@@ -38,49 +38,43 @@ const createStore = () => {
             {icon: 'https://cdn.svgporn.com/logos/css-3.svg',},]},
       ],
       dynamics: [
-        {id: '0',title: 'Bingo',link: '/dynamics/',to:'https://js-bingo.netlify.app/',img: '/images/js/Bingo.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id: 0,title: 'Bingo',link: '/dynamics/',to:'https://js-bingo.netlify.app/',img: '/images/js/Bingo.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-        {id: '1',title: 'Slot',link: '/dynamics/',to:'https://js-slot.netlify.app/',img: '/images/js/Slot.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id: 1,title: 'Slot',link: '/dynamics/',to:'https://js-slot.netlify.app/',img: '/images/js/Slot.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {
               title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-        {id: '2',title: 'SlideShow',link: '/dynamics/',to:'https://js-slideshow.netlify.app/',img: '/images/js/SlideShow.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id:2,title: 'SlideShow',link: '/dynamics/',to:'https://js-slideshow.netlify.app/',img: '/images/js/SlideShow.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-        {id: '3',title: 'Watch',link: '/dynamics/',to:'https://js-watch.netlify.app/',img: '/images/js/Watch.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id: 3,title: 'Watch',link: '/dynamics/',to:'https://js-watch.netlify.app/',img: '/images/js/Watch.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-        {id: '4',title: 'ECstore',link: '/dynamics/',to:'',img: '/images/js/Bingo.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id: 4,title: 'ChoicesQuestion',link: '/dynamics/',to:'https://js-questions.netlify.app/',img: '/images/js/ChoicesQuestions.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-        {id: '5',title: 'ChoicesQuestion',link: '/dynamics/',to:'https://js-questions.netlify.app/',img: '/images/js/ChoicesQuestions.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id: 5,title: 'Typing',link: '/dynamics/',to:'https://js-typing.netlify.app/',img: '/images/js/Typing.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-        {id: '6',title: 'Typing',link: '/dynamics/',to:'https://js-typing.netlify.app/',img: '/images/js/Typing.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
-            {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
-            {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]
-        },
-        {id: '7',title: 'Calender',link: '/dynamics/',to:'https://js-calender.netlify.app/',img: '/images/js/Calender.png' , useLangs: [
-            {title: "HTML", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
-            {title: "CSS", width: "width:15px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
+        {id: 6,title: 'Calender',link: '/dynamics/',to:'https://js-calender.netlify.app/',img: '/images/js/Calender.png' , useLangs: [
+            {title: "HTML", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/html-5.svg",},
+            {title: "CSS", width: "width:20px;", icon: "https://cdn.svgporn.com/logos/css-3.svg",},
             {title: "SASS", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/sass.svg",},
             {title: "JavaScript", width: "width:30px;", icon: "https://cdn.svgporn.com/logos/javascript.svg",},]},
-      ]
+      ],
     },
     mutations: {
 
@@ -89,7 +83,12 @@ const createStore = () => {
 
     },
     getters: {
-
+      getDynamicsData: state => id => {
+        return state.dynamics.find(data => data.id == id)
+      },
+      getStaticsData: state => id => {
+        return state.statics.find(data => data.id == id)
+      },
     },
   })
 }
